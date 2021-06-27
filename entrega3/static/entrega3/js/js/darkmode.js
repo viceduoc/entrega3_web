bb86ee3b3821700ac92285bb4c9cb8c819e8caa1
Vicente Carmona <vice.carmona@duocuc.cl>
2021-06-27 14:29:44 -0400

    Creación de mostrar NOTICIAS. Primer parte del crud

--- entrega3/static/entrega3/js/js/darkmode.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#night-mode").click(function (){
-        var element = document.body;         
-        element.classList.toggle("dark"); 
-        $('.card').toggleClass("dark");
-        $('.modal-content').toggleClass("darkmodal");
-    });
-});
--- entrega3/views.py
+++ entrega3/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Noticia
 
 # Create your views here.
 def home(request):
@@ -7,3 +8,13 @@ def home(request):
 # agrengando segundo template al view 
 def formulario(request):
     return render(request, 'entrega3/formulario.html')
+
+
+
+def noticias(request):
+    noticias = Noticia.objects.all()
+    datos = {
+        'noticias' : noticias
+    }
+    return render(request, 'entrega3/noticias.html', datos)
+
