d0757fceefd08dfcf0d46b6cd8de46959b45dc6b
Patricio Mardones <pa.mardonesg@duocuc.cl>
2021-06-27 14:54:52 -0400

    Revert "Merge branch 'Dev' into vicente"
    This reverts commit be069f416c6ad6f6a6cb56719623fb4baac84388, reversing
    changes made to bb86ee3b3821700ac92285bb4c9cb8c819e8caa1.

--- entrega3/views.py
+++ entrega3/views.py
@@ -1,9 +1,5 @@
-
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from .models import Noticia
-# Agregado HttpResponse revisar sin funcona de lo contrario eliminar
-# 
-
 
 # Create your views here.
 def home(request):
@@ -14,9 +10,11 @@ def formulario(request):
     return render(request, 'entrega3/formulario.html')
 
 
+
 def noticias(request):
     noticias = Noticia.objects.all()
     datos = {
         'noticias' : noticias
     }
     return render(request, 'entrega3/noticias.html', datos)
+
